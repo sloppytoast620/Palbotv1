41971127dd7e1a3f54b884c55955e1a370a8aca0
sloppytoast620 <sloppytoast620@gmail.com>
2026-02-04 00:22:23 -0500

    Fix variable name from 'token' to 'TOKEN'

--- palbot/config.py
+++ palbot/config.py
@@ -3,6 +3,6 @@ import os
 # Railway provides this variable directly to the OS
 TOKEN = os.environ.get("DISCORD_TOKEN")
 
-if token is None:
+if TOKEN is None:
     # This helps catch the TypeError before it happens
     raise ValueError("MY_TOKEN is not set in Railway variables!")
