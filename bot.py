a17b817efe0de3af8577217fcdf3d64fe3976650
sloppytoast620 <sloppytoast620@gmail.com>
2026-02-03 23:08:24 -0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,20 @@
+
+import discord
+from discord.ext import commands
+from palbot.config import TOKEN
+from palbot.storage import init_db
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    await bot.tree.sync()
+
+@bot.event
+async def setup_hook():
+    init_db()
+    await bot.load_extension("palbot.cogs.game")
+
+bot.run(TOKEN)
